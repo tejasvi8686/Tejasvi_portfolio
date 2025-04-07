6acd51c320824135a0437770e6c223fd29e4da17
Tejasvi raj <105453831+tejasvi8686@users.noreply.github.com>
2025-04-07 19:46:44 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -94,7 +94,7 @@ const Home = () => {
                       className="underline"
                       download
                       target="_blank"
-                      to="https://drive.google.com/file/d/1Wd3LEf-lNbHuf0An_4rKY_M67tY5bQCD/view?usp=sharing"
+                      to="https://drive.google.com/file/d/13EQ615IeSuwfotX-DwqASMGNNovTq6Od/view?usp=drivesdk"
                     >
                       https://docs.google.com/resume/
                     </Link>
